bf94e29413a8bd0920c8a8dcaa2e0e17baab7a32
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-07-05 16:55:43 -0300

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Frontend + backend dev environment";
+  description = "Dev Environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,19 +10,70 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+        python = pkgs.python312;
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            python312
+            python
             uv
             nodejs_22
             corepack
+            docker
+            kubectl
+            kustomize
+            helm
             git
           ];
 
           shellHook = ''
-            echo "Dev shell ready"
+            echo "Dev shell loaded"
+            export UV_PYTHON=${python}/bin/python
           '';
         };
+
+        packages.backend = pkgs.stdenv.mkDerivation {
+          pname = "backend";
+          version = "0.1.0";
+          src = ./backend;
+
+          nativeBuildInputs = [
+            python
+            pkgs.uv
+          ];
+
+          buildPhase = ''
+            echo "Backend build placeholder"
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r . $out/
+          '';
+        };
+
+        packages.frontend = pkgs.stdenv.mkDerivation {
+          pname = "frontend";
+          version = "0.1.0";
+          src = ./frontend;
+
+          nativeBuildInputs = [
+            pkgs.nodejs_22
+            pkgs.corepack
+          ];
+
+          buildPhase = ''
+            corepack enable
+            npm install
+            npm run build
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r dist $out/
+          '';
+        };
+
+        packages.default = self.packages.${system}.backend;
       });
 }
