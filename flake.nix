aaf8326e21372f360a3fa27d0488ea55d1791959
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-07-05 16:38:06 -0300

    init backend flake test

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Frontend + backend dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            uv
+            nodejs_22
+            corepack
+            git
+          ];
+
+          shellHook = ''
+            echo "Dev shell ready"
+          '';
+        };
+      });
+}
