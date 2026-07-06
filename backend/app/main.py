9fbe5197d191638bfe76bc96bacf46fdf1b03daf
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-07-06 09:58:49 -0300

    implement v1 prefix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+app = FastAPI(
+    title="Blog API",
+    version="0.0.1",
+    description="Blog endpoints",
+    docs_url="/docs",
+)
+
+v_router = APIRouter(
+    prefix="/v1",
+    tags=["v1"],
+)
 
 
 @app.get("/health")
