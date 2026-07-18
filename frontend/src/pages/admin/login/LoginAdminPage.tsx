4df4d8fe0b37c3239af4505933b2fdb5fdcdfea4
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-07-17 22:56:05 -0300

    fix css layout

--- frontend/src/pages/admin/login/LoginAdminPage.tsx
+++ frontend/src/pages/admin/login/LoginAdminPage.tsx
@@ -1,36 +1,51 @@
-import { useLoginForm } from './useLoginForm';
-import { useAuth } from '../../../contexts/AuthContext';
-import "./LoginAdmin.css"
+import { useLoginForm } from "./useLoginForm";
+import { useAuth } from "../../../contexts/AuthContext";
+import "./LoginAdmin.css";
 
 function LoginAdmin() {
-  const { username, handleUsernameChange,
-    password, handlePasswordChange,
-    onSubmit, errors,
-  } = useLoginForm()
-  const { loading } = useAuth()
-
+  const {
+    username,
+    handleUsernameChange,
+    password,
+    handlePasswordChange,
+    onSubmit,
+    errors,
+  } = useLoginForm();
+  const { loading } = useAuth();
 
   return (
     <div className="admin-login-container page">
       <h1>Admin</h1>
       {errors.server ? <p className="error">{errors.server}</p> : null}
-      <form onSubmit={onSubmit} >
-        <label>
-          Username:
-          <input className={errors.username ? "inputErrors" : ""} name="username" value={username} onChange={handleUsernameChange} />
-        </label>
-        {errors.username ? <p className="error">{errors.username}</p> : null}
+      <form onSubmit={onSubmit}>
+        <div className="input-login">
+          <p>Username:</p>
+          <input
+            className={errors.username ? "inputErrors" : ""}
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          {errors.username ? <p className="error">{errors.username}</p> : null}
+        </div>
 
-        <label>
-          Password:
-          <input className={errors.password ? "inputErrors" : ""} name="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        {errors.password ? <p className="error">{errors.password}</p> : null}
+        <div className="input-login">
+          <p>Password: </p>
+          <input
+            className={errors.password ? "inputErrors" : ""}
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          {errors.password ? <p className="error">{errors.password}</p> : null}
+        </div>
 
-        <button type='submit' disabled={loading}  >Login</button>
+        <button type="submit" disabled={loading}>
+          Login
+        </button>
       </form>
-    </div >
-  )
+    </div>
+  );
 }
 
 export default LoginAdmin;
