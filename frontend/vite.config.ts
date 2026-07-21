0869cbdfe14dc808eb300d261a98027f339fbe86
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-07-21 01:03:20 -0300

    fix network docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,12 +12,12 @@ export default defineConfig({
         target: "http://backend:8000",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    }
-  }
-})
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
